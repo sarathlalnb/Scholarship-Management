2d73c566fe3e994997b9cee40e6bb2b97748246f
Nabeelkn <nabeelkn07@gmail.com>
2024-05-10 10:50:05 +0530

    3rd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Home from './Pages/Home'
+import Landing from './Pages/Landing'
 
 function App() {
  
 
   return (
     <>
-    
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/refister' element={<Register/>}/>
+      <Route path='/' element={<Landing/>}/>
+      <Route path='/home' element={<Home/>}/>
+
+    </Routes>
     </>
   )
 }
