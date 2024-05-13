3a3dc2f8ce8a56ebd1d841cba04293961827f71a
jithu291 <sreejithjithu291@gmail.com>
2024-05-13 14:09:53 +0530

    landing

--- src/main.jsx
+++ src/main.jsx
@@ -8,5 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    </BrowserRouter>
+  </React.StrictMode>
 )
