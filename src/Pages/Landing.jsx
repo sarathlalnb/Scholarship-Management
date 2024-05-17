d8cc8d2b79bec239a45e71038b694f1830469c30
jithu291 <sreejithjithu291@gmail.com>
2024-05-17 10:58:13 +0530

    footer1

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -172,7 +172,7 @@ const scrollToAbout =()=>{
       </div>
       
 {/* Footer */}
-<footer class="bg-[#EEEEEE] dark:bg-gray-900 ">
+<footer class="bg-[#EEEEEE]  ">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
